878ee97bee8ee62948c7c38ea1aad87f9a700903
blackbird <blackbird38fr@gmail.com>
2019-10-19 22:37:09 +0200

    added a migration: user admin (admin@admin.org, pass: admin)

---
+++ src/Migrations/Version20191019202634.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191019202634 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+        $encoded = password_hash('admin', PASSWORD_ARGON2I);
+        $now = new \DateTime();
+        $now  = $now ->format('Y-m-d H:i:s');
+        $user = array('encoded' => $encoded, 'now' => $now);
+        $this->addSql('INSERT INTO user(`id`, `email`, `roles`, `password`, `name`, `last_name`, `phone`, `photo`, `created_at`) VALUES (null,"admin@admin.org",\'["ROLE_ADMIN", "ROLE_USER"]\',:encoded,"admin","admin","admin","+33126273849",:now)', $user);
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
