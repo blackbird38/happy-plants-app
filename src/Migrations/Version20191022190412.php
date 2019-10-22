d75e9d7495e3c8a88cbfdf33914341b4167bcf98
blackbird <blackbird38fr@gmail.com>
2019-10-22 21:18:59 +0200

    correction: migration and twig

--- src/Migrations/Version20191022190412.php
+++ src/Migrations/Version20191022190412.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191020213831 extends AbstractMigration
+final class Version20191022190412 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20191020213831 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE species ADD photo VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE species ADD created_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20191020213831 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-       // $this->addSql('ALTER TABLE species DROP photo');
+        $this->addSql('ALTER TABLE species DROP created_at');
     }
 }
